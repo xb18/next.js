7b3d0ea3844e45fb016ea59f76a2905db720d2e4
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2026-08-21 14:03:53 +0200

    More granular cache keys for use-cache entries: env

---
+++ test/production/app-dir/use-cache-code-hash/app/env-dynamic/logic.tsx
@@ -0,0 +1,9 @@
+globalThis.blackbox = (v) => ({ DECOY: v.FOOBAR })
+
+// @ts-ignore
+const envVar = blackbox(process.env).DECOY
+
+export async function logic() {
+  'use cache'
+  return `${envVar}`
+}
