ec45440f3ad2f4154e81e4e66439f8fe1f3c6c30
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2026-08-21 15:19:44 +0200

    review: cleanup

--- turbopack/crates/turbopack-core/src/module_graph/mod.rs
+++ turbopack/crates/turbopack-core/src/module_graph/mod.rs
@@ -1952,12 +1952,9 @@ impl Visit<SingleModuleGraphBuilderNode, RefData> for SingleModuleGraphBuilder<'
                     let _span = span.entered();
                     span = tracing::info_span!("per-entry reference");
                 }
-                ChunkingType::Emitted {
-                    namespace: merge_tag,
-                    ..
-                } => {
+                ChunkingType::Emitted { namespace, .. } => {
                     let _span = span.entered();
-                    span = tracing::info_span!("emitted reference", merge_tag = debug(&merge_tag));
+                    span = tracing::info_span!("emitted reference", namespace = debug(&namespace));
                 }
                 ChunkingType::Collected { namespace, .. } => {
                     let _span = span.entered();
